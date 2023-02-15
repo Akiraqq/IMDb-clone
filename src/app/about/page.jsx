1e8ea95dce47bb2211b41d05daf5b31b70ff8dca
Akira <dzirt1907@rambler.ru>
2023-02-16 00:14:52 +0200

    Create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,76 @@
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 px-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, commodi
+        nemo aliquid, quas officia enim cum, nisi ipsum sequi deleniti deserunt
+        labore in eius quod? Consequuntur eius odio amet! Aut consequuntur nobis
+        recusandae fugiat. Labore, repellat quia, perferendis hic quae sequi
+        assumenda at harum exercitationem alias esse voluptas, atque odio ab
+        enim ex a quibusdam voluptatum quo ea fuga laborum ratione soluta odit.
+        Sed consequatur harum praesentium? Cumque aliquam temporibus nobis
+        consequatur eum exercitationem magnam illum cum pariatur neque.
+        Molestias voluptates assumenda repudiandae perspiciatis, expedita esse
+        nulla reprehenderit consectetur incidunt vitae necessitatibus, numquam
+        quibusdam suscipit, facilis minus repellendus maiores totam.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
+        tenetur itaque culpa rerum placeat ducimus nulla minima quae. Architecto
+        aut pariatur excepturi numquam fugiat eius obcaecati, libero quo
+        blanditiis nihil voluptate aperiam, repudiandae consectetur
+        perspiciatis. Quaerat ut error maxime earum, rem enim nobis voluptas id
+        doloremque harum at voluptates numquam sed corporis rerum vero fugit
+        pariatur distinctio assumenda nam quos? Odio veritatis earum omnis a
+        consequatur sunt veniam cupiditate ex? Animi beatae ut nam in nihil
+        itaque temporibus unde accusamus quasi magnam maiores quas iure
+        doloremque nesciunt fugiat adipisci possimus, sit impedit tempore sint
+        recusandae ad? Minus cupiditate inventore natus deserunt dolor obcaecati
+        dolore? Explicabo tempore, maiores eos quo soluta iure debitis ea saepe
+        vero dignissimos vel, et doloribus dolorum amet? Sed sequi dolorem
+        quasi, velit animi ab blanditiis. Sequi libero illum corrupti
+        consectetur, inventore laudantium! Excepturi amet magnam exercitationem
+        inventore nihil. Sunt molestiae ducimus cupiditate a recusandae odit
+        aliquam. Sint maiores sunt, enim, at error dolorum voluptatibus ullam
+        culpa eligendi non obcaecati, voluptatem eius. Aliquid placeat aperiam
+        alias laboriosam unde voluptatum qui maxime, fugiat ea neque quos sit
+        quasi. Ducimus eligendi natus veniam repudiandae impedit. Quisquam in
+        ipsam voluptatibus facere fugit sequi consequuntur, maxime consectetur
+        sunt neque harum soluta!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur
+        quisquam quidem, ab dolore fuga facere, nostrum accusantium aliquid,
+        perspiciatis ea veritatis? Quia debitis nihil libero reiciendis tempora
+        obcaecati saepe dicta nam quasi facilis eligendi quas accusamus
+        corrupti, sit pariatur aliquam molestias aperiam temporibus! Aspernatur,
+        soluta debitis. Cumque, asperiores fugiat! Totam, nulla facilis
+        perspiciatis quisquam eius fuga, doloribus laboriosam earum rerum
+        reprehenderit sint nisi neque dolor alias ipsam officia repellat soluta
+        dolores expedita temporibus error accusamus. Accusantium mollitia
+        perspiciatis asperiores aut eligendi, commodi nisi delectus aperiam
+        incidunt eaque quia. Natus nostrum ipsam quo id repellendus quas aut
+        facere deleniti reprehenderit corporis tempora, minus ratione. Ipsam
+        culpa cupiditate praesentium possimus vero eum, aperiam neque est nulla
+        quos similique quisquam quidem delectus, quia cum eaque facere ad
+        aliquid. Esse porro fugiat culpa earum consequatur? Dicta, doloremque
+        eligendi et magni nemo, quo eaque fuga consequuntur tempora aperiam
+        fugiat neque enim repellendus magnam tempore rem.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti ex
+        incidunt a at nihil, nobis numquam eveniet ut quos magni molestiae
+        voluptas harum obcaecati repellendus debitis similique porro libero odit
+        nemo animi accusantium, impedit laboriosam rerum commodi. Provident
+        iusto asperiores optio! Ipsa numquam ducimus sit nostrum illo quasi
+        accusamus laboriosam error impedit enim aspernatur iusto, adipisci eaque
+        voluptas nisi nulla accusantium provident quod excepturi consequatur?
+        Fuga doloremque eius est exercitationem nemo magnam dolorem minima aut,
+        sunt suscipit voluptates rem?
+      </p>
+    </div>
+  )
+}
+
+export default About
