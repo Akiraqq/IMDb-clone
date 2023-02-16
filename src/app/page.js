646e8600f5ae1b29e296511088898a3769258203
Akira <dzirt1907@rambler.ru>
2023-02-16 02:02:52 +0200

    fatch data in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <h1>HOME</h1>
+const Home = async ({ searchParams }) => {
+  const genre = searchParams.genre || 'fetchTrending'
+  const res = await fetch(
+    `http://api.themoviedb.org/3/${
+      genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'
+    }?api_key=${process.env.API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  )
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  const data = await res.json()
+  const results = data.results
+
+  return (
+    <div>
+      {results.map((res) => {
+        return <div key={res.id}> {res.title}</div>
+      })}
+    </div>
+  )
 }
+
+export default Home
