c25ec80924b3e3a4607f5e4137c0efcd0072208f
Akira <dzirt1907@rambler.ru>
2023-02-16 04:36:29 +0200

    Add card component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import Card from '@/components/Card'
+
 const Home = async ({ searchParams }) => {
   const genre = searchParams.genre || 'fetchTrending'
   const res = await fetch(
@@ -13,9 +15,9 @@ const Home = async ({ searchParams }) => {
   const results = data.results
 
   return (
-    <div>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 max-w-6xl mx-auto py-4 px-2">
       {results.map((res) => {
-        return <div key={res.id}> {res.title}</div>
+        return <Card key={res.id} results={res} />
       })}
     </div>
   )
---
+++ src/components/Card.jsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { FiThumbsUp } from 'react-icons/fi'
+
+const Card = ({ results }) => {
+  return (
+    <Link
+      href={`/movie/${results.id}`}
+      className="cursor-pointer sm: p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group"
+    >
+      <Image
+        src={`http://image.tmdb.org/t/p/original/${
+          results.backdrop_path || results.poster_part
+        }`}
+        width={500}
+        height={300}
+        className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
+        placeholder="blur"
+        blurDataURL="/spinner.svg"
+      />
+
+      <div className="p-2">
+        <p className="line-clamp-2">{results.overview}</p>
+        <h2 className="truncate text-lg font-bold ">
+          {results.title || results.name}
+        </h2>
+        <div className="flex items-center justify-between mt-2">
+          <p>{results.release_date || results.first_air_date}</p>
+          <p className="flex items-center gap-1">
+            <FiThumbsUp /> {results.vote_count}
+          </p>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default Card
